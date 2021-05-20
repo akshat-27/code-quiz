5fdc70354f7203b49b527c285569b2c8a114e965
akshat-27 <75124367+akshat-27@users.noreply.github.com>
2021-05-20 16:00:52 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C O T I U F N N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("S B D A A A E T", 100,350)
+    fill("lightblue")
+    text("Hint: Stores all the information", 100,370)
+
+}
